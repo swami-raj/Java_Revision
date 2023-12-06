b78d61a173f90da4ec05243001be4f2c60e95912
swami raj <swamiraj0303@gmail.com>
2023-12-07 00:47:29 +0530

    8 dec

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,36 @@
 public class Main {
+    static class Employee{
+        int id;
+        String name;
+        int Salary;
+        public void printdetail(){
+            System.out.println("my id is: " + id);
+            System.out.println("my name is: " + name);
+        }
+        public int getSalary(){
+            return Salary;
+        }
+
+    }
     public static void main(String[] args) {
+        Employee slayer= new Employee();   // Instantiating a new Employee Object
+        Employee viper= new Employee();
 
-        System.out.println("Hello world!");
-        hfdsfhasjjfah
-                afa
+        //setting attribute for slayer
+        slayer.id=12;
+        slayer.Salary=123312;
+        slayer.name="rollins";
 
+        //setting attribute for viper
+        viper.id=11;
+        viper.Salary=12321;
+        viper.name="orton";
 
+        //printing the attribute
+        slayer.printdetail();
+        int Salary=slayer.getSalary();
+        System.out.println(Salary);
 
-                a
+        viper.printdetail();
     }
 }
